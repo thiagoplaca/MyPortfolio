18d11a9e638df820538b9557343db95e30c0645e
Thiago <thiago-placa@hotmail.com>
2023-05-17 21:08:21 -0300

    Atualizando

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,24 +1,26 @@
 import { motion } from "framer-motion";
+
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
 
-const SectionWrapper = (Component, idName) => 
-    function HOC() {
-        return (
-            <motion.section
-                variants={staggerContainer()}
-                initial='hidden'
-                whileInView='show'
-                viewport={{ once: true, amount: 0.25 }}
-                className={`${styles.padding} max-w-7xl mx-auto relative z-0 `}>
-                    <span className="hash-span" id={idName}>
-                      &nbsp; 
-                    </span>
-                <Component />
-            </motion.section>
-        )
-    }
+const StarWrapper = (Component, idName) => {
+  return function HOC({ worksInitial = 'hidden', ...props }) {
+    return (
+      <motion.section
+        variants={staggerContainer()}
+        initial={props.hasOwnProperty('worksInitial') ? worksInitial : 'visible'}
+        whileInView='show'
+        viewport={{ once: true, amount: 0.25 }}
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+        <span className='hash-span' id={idName}>
+          &nbsp;
+        </span>
 
+        <Component {...props} />
+      </motion.section>
+    );
+  };
+};
 
-
-export default SectionWrapper
+export default StarWrapper;
