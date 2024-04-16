0c1afdf962f90c73af7daac744be3b17ee1f8ffb
Thiago Placa <thiago_placa99@outlook.com>
2024-04-16 20:10:02 -0300

    Excluindo ComputerCanvas

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import { motion } from 'framer-motion'
 import { Tilt } from 'react-tilt'
 import { styles } from '../styles'
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
 import { motion } from 'framer-motion'
 
 import { styles } from '../styles'
-import { ComputersCanvas } from './canvas'
+
 
 const Hero = () => {
   return (
@@ -20,7 +19,6 @@ const Hero = () => {
         </div>
 
       </div>
-        <ComputersCanvas />
 
         <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
           <a href="#about">
